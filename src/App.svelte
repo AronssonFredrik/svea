ea2fc5858705a632fd6be9143b670bd0a09ef715
AronssonFredrik <fredrik.e.aronsson@gmail.com>
2019-10-17 22:18:27 +0200

    build(git): initiate

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let name;
+  import Button from "./components/Button.svelte";
+
+	let handleClickEvent = (event) => {
+		console.table(event.detail.event);
+	}
+
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {name}!</h1>
+
+<Button text="hello abcf"
+	on:clickHandle={handleClickEvent}
+	>lorem, ipsum</Button>
