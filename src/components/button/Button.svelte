4544e83b9baa767928ec19586d6b903cd675a260
Fredrik Aronsson <fredrik.e.aronsson@gmail.com>
2019-10-21 16:48:36 +0200

    uppdatering

---
+++ src/components/button/Button.svelte
@@ -0,0 +1,44 @@
+<script>
+  // logic goes here
+  export let text,
+    color,
+    outline,
+    disabled = false;
+
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  let handleClick = event => {
+    dispatch("clickHandle", {
+      event: event
+    });
+  };
+  
+</script>
+
+<style>
+  /* styles go here */
+  /*
+    default
+    primary
+    secondary
+  */
+  button {
+    
+  }
+</style>
+
+<!-- markup goes here -->
+<button
+  on:click={handleClick}
+  {disabled}
+  class={color ? `color-${color}` : 'color-default'}
+  class:outline={outline === 'true'}>
+  <slot>{text}</slot>
+</button>
+
+<!-- <button on:click={handleClick}
+  disabled="{{disabled}}">
+  <slot>{text}</slot>
+</button> -->
