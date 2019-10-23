c22892690eaac94c74228bbf24354f8535edeb49
Fredrik Aronsson <fredrik.e.aronsson@gmail.com>
2019-10-23 08:30:00 +0200

    fix(config): sass preprocessor

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import sass from 'node-sass';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -16,15 +17,34 @@ export default {
 	},
 	plugins: [
 		svelte({
-			// enable run-time checks when not in production
 			dev: !production,
 			// we'll extract any component CSS out into
 			// a separate file — better for performance
 			css: css => {
 				css.write('public/bundle.css');
+			},
+			preprocess: {
+				style: ({ content, attributes }) => {
+					if (attributes.type !== 'text/scss') return;
+
+					return new Promise((fulfil, reject) => {
+						sass.render({
+							data: content,
+							includePaths: ['src'],
+							sourceMap: true,
+							outFile: 'x' // this is necessary, but is ignored
+						}, (err, result) => {
+							if (err) return reject(err);
+
+							fulfil({
+								code: result.css.toString(),
+								map: result.map.toString()
+							});
+						});
+					});
+				}
 			}
 		}),
-
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
 		// some cases you'll need additional configuration —
