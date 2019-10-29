8c58f44714ef39a3ece101b945b57a6e6aa5421c
Fredrik Aronsson <fredrik.e.aronsson@gmail.com>
2019-10-29 08:42:17 +0100

    fix(storybook): now includes addons for notes, a11y and knobs

--- stories/button/button.stories.js
+++ stories/button/button.stories.js
@@ -1,26 +1,28 @@
 import BtnStyle from './BtnStyle.svelte';
+import Button from "../../src/components/Button.svelte";
 import { action } from '@storybook/addon-actions';
 
+import ButtonReadme from './README.md';
 
 export default {
-    title: 'Btn',
+  title: 'Button',
+  parameters: {
+    notes: { markdown: ButtonReadme }
+  }
 };
 
 export const primary = () => ({
-        Component: BtnStyle,
-        props: { text: 'Hello' }
-});
-  
-  export const secondary = () => ({
-        Component: BtnStyle,
-        props: { text: 'Hello' }
+  Component: BtnStyle,
+  props: { text: 'Hello'}  
 });
 
-export const text = () => 
-  (
-    {
-    Component: BtnStyle,
-    props: { text: 'Hello Button' },
-    on: { click: action('clicked') },
-  }
-);
+export const secondary = () => ({
+  Component: BtnStyle,
+  props: { text: 'Hello' }
+});
+
+export const text = () => ({
+  Component: Button,
+  props: { text: 'Hello Button' },
+  on: { click: action('click') },
+});
