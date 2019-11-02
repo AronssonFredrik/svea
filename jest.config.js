9f523881603a12050cbf67e7a4c7789a96524a88
Fredrik Aronsson <fredrik.e.aronsson@gmail.com>
2019-11-02 18:26:20 +0100

    fix(jest): update config to preprocess scss

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,35 @@
+const sass = require('node-sass');
+
+
 module.exports = {
     transform: {
       '^.+\\.js$': 'babel-jest',
-      '^.+\\.svelte$': 'jest-transform-svelte'
+      '^.+\\.svelte$': 
+        [
+          'jest-transform-svelte',
+          {
+            preprocess: {
+              style: ({ content, attributes }) => {
+                if (attributes.type !== 'text/scss') return;
+                return new Promise((fulfil, reject) => {
+                  sass.render({
+                    data: content,
+                    includePaths: ['src'],
+                    sourceMap: true,
+                    outFile: 'x' // this is necessary, but is ignored
+                  }, (err, result) => {
+                    if (err) return reject(err);
+        
+                    fulfil({
+                      code: result.css.toString(),
+                      map: result.map.toString()
+                    });
+                  });
+                });
+              }
+            }        
+          }
+        ]
     },
     moduleFileExtensions: ['js', 'svelte'],
     bail: false,
